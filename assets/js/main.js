4f49186430a0790fe56586d0211dc46eb21e2f6c
Parth Lakare <151749339+paarrtthh@users.noreply.github.com>
2026-06-17 14:46:44 +0530

    Refactor and enhance menu and theme functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,8 +49,24 @@ const skillsContent = document.getElementsByClassName('skills__content'),
     })
 
 /*==================== QUALIFICATION TABS ====================*/
-     
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualifications__active')
+        })
+        tab.classList.add('qualifications__active')
+    })
+})
 /*==================== SERVICES MODAL ====================*/
 
 
